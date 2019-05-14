a678e4a6a7d9358915e057ee1c89c0a93abbff3d
Lydia Yuzva <lydiaysd@gmail.com>
2019-05-14 09:35:03 +0100

    Rails-yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:show, :index, :new, :create]
+  end
+  # get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
+  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
+  # get "reviews/:id",                            to: "reviews#show"
 end
+
